d9fd69646f62a7f2c8d4b963adfd7a37e4a28198
Riley Lathan <rileylatha202@gmail.com>
2021-06-29 18:16:16 -0400

    initial files and structure

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+    {
+        title: "Why do JavaScript and Java have similar name?",
+        choice: ["JavaScript is a stripped-down version of Java", "JavaScript's syntax is loosely based on Java's", "They both originated on the island of Java", "None of the above"],
+        answer: "JavaScript's syntax is loosely based on Java's"
+    },
+    {
+        title: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        choice: ["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"],
+        answer: "The User's machine running a Web browser"
+    },
+    {
+        title: "______ JavaScript is also called client-side JavaScript.",
+        choice: ["Microsoft", "Navigator", "LiveWire", "Native"],
+        answer: "Navigator"
+    },
+    {
+        title: "__________ JavaScript is also called server-side JavaScript.",
+        choice: ["Microsoft", "Navigator", "LiveWire", "Native"],
+        answer: "LiveWire"
+    },
+    {
+        title: "What are variables used for in JavaScript Programs?",
+        choice: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+        answer: "Storing numbers, dates, or other values"
+    },
+    {
+        title: "_____ JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and page navigation.",
+        choice: ["Client-side", "Server-side", "Local", "Native"],
+        answer: "Client-side"
+    },
+]
+
+console.log(questions[0].choice[0]);
